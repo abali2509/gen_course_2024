4e9be38cf9ab2d74e01ee052c6a42afa55337a3b
mukhtar <mukhtar_yusuf@hotmail.co.uk>
2024-08-31 23:23:36 +0100

    Added all the new files

---
+++ src/utils/menu.py
@@ -0,0 +1,17 @@
+
+# This is the main menu 
+
+main_menu = """
+                Main Menu
+                0 - Exit 
+                1 - Product Menu
+                """
+
+product_menu = """
+                Product Menu
+                0 - Return To Main Menu
+                1 - List Products 
+                2 - Create New Products 
+                3 - Update Products
+                4 - Delete Products
+                """
