5a5b76fa78bd75ba56bdee04c18d8ee68d8e583d
mukhtar <mukhtar_yusuf@hotmail.co.uk>
2024-09-01 23:54:40 +0100

    I have added the new files we worked on

--- reader_test.py
+++ reader_test.py
@@ -7,18 +7,29 @@ def open_file(path:str)-> list:
 
     for line in file.readlines():
         file_contents.append(line.rstrip())
-
+    file.close()
     return file_contents
     
 
 # write 
 
+def write_anything(file_contents: list, path: str)-> None:
+    
+    with open(path, "r+") as file:
+        for l in file_contents:
+            file.write(f'{l}\n')
+        # file.write('Hello')
 
+    
+    
+    
 
 
 products = open_file("./data/txt_files/products.txt")
-
-print(products)
+products.remove('Water')
+write_anything(products, "./data/txt_files/products.txt")
+# for product in products:
+#     print(product)
 # # openning a file
 # products = open("./data/txt_files/products.txt", "+r")
 
